192919e5553837b76c14f3d34c056a1557ae9f6b
FloorFranken <101178001+FloorFranken@users.noreply.github.com>
2022-03-11 09:22:57 +0100

    Add files via upload

--- Optie2_diabetes-Copy1.py
+++ Optie2_diabetes-Copy1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[25]:
+# In[330]:
 
 
 import matplotlib.pyplot as plt
@@ -15,55 +15,55 @@ from sklearn.metrics import confusion_matrix
 import streamlit as st
 
 
-# In[26]:
+# In[331]:
 
 
 # Titel en subtitel van de app
 
 st.title('Diabetes detectie')
-st.header('Bepaal de kans op diabetes middels deze app')
+st.header('Kijk of u diabetes heeft met behulp van deze app')
 
 # Teskst behorende bij de app
 
-st.text('Diabetes is een chronische stofwisselingsziekte. Bij diabetes zit er te veel suiker in het bloed. Het lichaam kan de bloedsuiker niet op peil houden. Met behulp van deze machine learning web app wordt het mogelijk om aan de hand van ingevoerde parameters een diagnose te maken over de mogelijkheid dat iemand diabetes heeft.De app maakt gebruik van historische data om de kans op diabetes te calculeren. Dit kan mensen helpen om betere en snellere diagnoses te maken of mensen helpen die geen tijd of geld hebben om een doctor te bezoeken.')
+st.write('Diabetes is een chronische stofwisselingsziekte. Bij diabetes zit er te veel suiker in het bloed. Het lichaam kan de bloedsuiker niet op peil houden. Met behulp van deze machine learning web app wordt het mogelijk om aan de hand van ingevoerde parameters een diagnose te maken over de mogelijkheid dat iemand diabetes heeft.De app maakt gebruik van historische data om de kans op diabetes te calculeren. Dit kan mensen helpen om betere en snellere diagnoses te maken of mensen helpen die geen tijd of geld hebben om een doctor te bezoeken.')
 
 #Voeg afbeelding toe > werkt alleen als de persoon die het upload naar streamlit zelf de afbeelding opslaat en filepath noteerd naar de afbeelding
 #Image = Image.open("C:\Users\joshua.bierenbrood\Documents\Data Science\Intro to datascience\Werkcollege week 3\diabetes.jpg")
 #st.image(image, caption = 'ML', use_column_width = True)
 
 
-# In[27]:
+# In[332]:
 
 
 df = pd.read_csv('diabetes.csv')
 
 
-# In[28]:
+# In[333]:
 
 
 st.title("Diabetes Database")
 
 
-# In[29]:
+# In[334]:
 
 
 df.head()
 
 
-# In[30]:
+# In[335]:
 
 
 df.info()
 
 
-# In[31]:
+# In[336]:
 
 
 df.eq(0).sum()
 #veel 0 waarden, vervangen door NaN en dan opvullen met gemiddelden
 
 
-# In[32]:
+# In[337]:
 
 
 df[['Glucose','BloodPressure','SkinThickness','Insulin','BMI',
@@ -71,80 +71,53 @@ df[['Glucose','BloodPressure','SkinThickness','Insulin','BMI',
     'SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']].replace(0,np.NaN)
 
 
-# In[33]:
+# In[338]:
 
 
 df.fillna(df.mean(), inplace=True)
 
 
-# In[34]:
+# In[339]:
 
 
 df.eq(0).sum() #checken
 
 
-# In[35]:
+# In[340]:
 
 
 df.describe()
 
 
-# In[36]:
+# In[341]:
 
 
 st.subheader('Informatie over de data:')
 st.dataframe(df)
 
 
-# In[37]:
+# In[342]:
 
 
-st.subheader('Aantal diabetesgevallen in dataset:')
-def countPlot():
-    fig = plt.figure()
-    sns.countplot(df["Outcome"])
-    st.pyplot(fig)
+st.title("Correlatie tussen de verschillende variabelen")
+st.subheader('In de onderstaande heatmap is te zien hoe de variabelen van elkaar afhangen.')
+fig, ax = plt.subplots()
+sns.heatmap(df.corr(), ax=ax, annot=True)
+st.write(fig)
 
 
-# In[38]:
+# In[343]:
 
 
-def histogram_pairplot():
-    st.header("Histogram & Pair Plot")
-    sd = st.selectbox(
-        "Selecteer een optie om data te visualiseren", 
-        [
-            "Histogram", 
-            "Pair Plot"   
-        ]
-    )
+st.title('Korte vraag tussendoor')
+option = st.selectbox(
+     'Hoe bent u bij deze app gekomen?',
+     ('Google', 'Mail', 'Link'))
 
-    fig = plt.figure()
-
-    if sd == "Histogram":
-        df.hist(bins = 50, figsize = (20,15))
-    
-    elif sd == "Pair Plot":
-        sns.pairplot(df, hue="Outcome")
-
-    st.pyplot(fig)
+st.write('U bent hier gekomen via', option)
 
 
-# In[39]:
-
-
-cor = df.corr()
-sns.heatmap(cor, annot = True)
-
-
-# In[40]:
-
-
-related = cor['Outcome'].sort_values(ascending=False)
-related
-
-
-# In[41]:
+# In[344]:
 
 
 #Machine learning
@@ -152,45 +125,47 @@ x_data= df.drop('Outcome',axis=1)
 y_data= df['Outcome']
 
 
-# In[42]:
+# In[345]:
 
 
 x_train, x_test, y_train, y_test = train_test_split(x_data,y_data, test_size=0.3, random_state=42)
 
 
-# In[43]:
+# In[346]:
 
 
 def get_input():
-    pregnancies = st.sidebar.slider('pregnancies', 0, 17,0)
-    glucose = st.sidebar.slider('glucose', 44, 199, 44)
-    blood_pressure = st.sidebar.slider('blood_pressure',24, 122,24)
-    skin_thickness = st.sidebar.slider('skin_thickness', 7, 99, 7)
-    insulin = st.sidebar.slider('insulin', 14.0, 846.0, 14.0)
+    pregnancies = st.sidebar.slider('Zwangerschappen', 0, 17,0)
+    glucose = st.sidebar.slider('Glucose', 44, 199, 44)
+    blood_pressure = st.sidebar.slider('Bloeddruk',24, 122,24)
+    skin_thickness = st.sidebar.slider('Huiddikte', 7, 99, 7)
+    insulin = st.sidebar.slider('Insuline', 14.0, 846.0, 14.0)
     BMI = st.sidebar.slider('BMI', 18.2, 67.1, 18.2)
     DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.078)
-    age = st.sidebar.slider('age', 21, 81, 21)
+    age = st.sidebar.slider('Leeftijd', 21, 81, 21)
     
-    user_data = {'zwangerschappen': pregnancies,
-                 'glucose': glucose,
-                 'bloeddruk': blood_pressure,
-                 'huiddikte': skin_thickness,
-                 'insuline': insulin,
+    user_data = {'Zwangerschappen': pregnancies,
+                 'Glucose': glucose,
+                 'Bloeddruk': blood_pressure,
+                 'Huiddikte': skin_thickness,
+                 'Insuline': insulin,
                  'BMI': BMI,
                  'DPF': DPF,
-                 'leeftijd': age
+                 'Leeftijd': age
                  }
     features = pd.DataFrame(user_data, index=[0])
     return features
 
 gekozen_input = get_input()
 
-st.subheader('Uw gekozen waarden:')
-st.write('selecteer de voor u geldende parameters in de slider boxen om de app de kans op diabetes te laten berekenen')
+st.subheader('Doe zelf de test of u diabetes heeft!')
+st.write('Selecteer de voor u geldende parameters in de sliderboxen aan de linkerkant om de app te laten bepalen of u diabetes heeft.')
                         
+st.subheader('Uw gekozen waarden:')
+st.write(gekozen_input)
 
 
-# In[44]:
+# In[347]:
 
 
 RandomForest=RandomForestClassifier()
@@ -199,29 +174,43 @@ Predict= RandomForest.predict(x_test)
 print(Predict)
 
 
-# In[45]:
+# In[348]:
 
 
 accuracyRFC = accuracy_score(y_test, Predict)
 print("Accuracy with Random Forrest Classification:", accuracyRFC)
 
 
-# In[46]:
+# In[349]:
 
 
 st.subheader('Accuratiescore bij het model')
 st.write(str(accuracy_score(y_test, Predict) * 100) + '%')
 
 
-# In[47]:
+# In[350]:
 
 
 diabetes_ja_nee = RandomForest.predict(gekozen_input)
 
 
-# In[48]:
+# In[351]:
 
 
-st.subheader('Wel (1) of geen (0) diabetes?')
+st.subheader('Wel of geen diabetes?')
+st.write("Wanneer de uitslag 1 is, heeft u wel diabetes. Wanneer de uitslag 0 is, heeft u geen diabetes.")
 st.write(diabetes_ja_nee)
+
+
+# In[352]:
+
+
+st.subheader('Heeft u volgens de test diabetes?')
+ja = st.checkbox('Ja')
+nee = st.checkbox('Nee')
+
+if ja:
+     st.write('Wat vervelend voor u..')
+if nee:
+    st.write('Wat fijn voor u!')
 
